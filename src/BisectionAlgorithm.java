d635f3b0cde9353dcde43b587e1c284a96e1ca5f
Krzychu <some@email.com>
2023-03-23 22:08:10 +0100

    Bisection done

--- src/BisectionAlgorithm.java
+++ src/BisectionAlgorithm.java
@@ -1,47 +1,42 @@
 public class BisectionAlgorithm {
 
+    static int i = 0;
 
-        public static double bisection(double a, double b, double epsilon) {
-            if (f(a) * f(b) >= 0) {
-                throw new IllegalArgumentException("Function has same signs at end points");
+    public static double bisection(double a, double b, double epsilon) {
+        if (f(a) * f(b) >= 0) {
+            throw new IllegalArgumentException("Function has same signs at end points");
+        }
+
+        double c = a;
+
+        while (Math.abs(f(c))>= epsilon) {
+            c = (a + b) / 2.0;
+            if (f(c) * f(a) < 0) {
+                b = c;
+            } else {
+                a = c;
             }
-
-
-            double c = a;
-
-            while ((b - a) >= epsilon) {
-                c = (a + b) / 2.0;
-
-                if (f(c) == 0.0) {
-                    break;
-                }
-                else if (f(c) * f(a) < 0) {
-                    b = c;
-                }
-                else {
-                    a = c;
-                }
-
-            }
-
-            return c;
+            i++;
         }
 
-        public static double f(double x) {
-            // Wprowadź swoją funkcję nieliniową
-            return x * x - 4 * x - 10;
-        }
-
-        public static void main(String[] args) {
-            double a = -2.0;
-            double b = 3.0;
-            double epsilon = 0.0001;
-
-
-            double root = bisection(a, b, epsilon);
-
-            System.out.println("Miejsce zerowe: " + root);
-        }
+        return c;
     }
+
+    public static double f(double x) {
+        // enter your function
+        return (x+1)*(Math.pow((x-1),4));
+    }
+
+    public static void main(String[] args) {
+        double a = -1.5;  //the left end of the interval
+        double b = -0.75;  // the rigt end of the interval
+        double epsilon = 0.000001; //
+
+
+        double result = bisection(a, b, epsilon);
+
+        System.out.println("x" + i + ": " + result);
+    }
+}
 
 
