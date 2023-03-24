d99f0fa9133b52c5d3804d81c7946c3766c694f2
Krzychu <krzychu123g@gmail.com>
2023-03-24 19:11:18 +0100

    sieczne zrobione 100%

---
+++ src/Sieczne.java
@@ -0,0 +1,69 @@
+public class Sieczne {
+    static int licznik = 1;
+
+    public static void sieczne(double a, double b, double epsil, double[] rownanie) {
+        if (func(rownanie, a) * func(rownanie, b) >= 0) {
+            throw new IllegalArgumentException("Wartosci na koncach przedzialu sa takie same!");
+        }
+        double[] f1 = pochodna(rownanie);
+        double[] f2 = pochodna(f1);
+        double x0 = a;
+        if (func(f1, a) * func(f2, a) >= 0) {
+            x0 = a;
+        }
+        if (func(f1, b) * func(f2, b) >= 0) {
+            x0 = b;
+        }
+
+        if (func(f1, b) * func(f2, b) >= 0 && func(f1, a) * func(f2, a) >= 0) {
+            if (func(rownanie, a) * func(f2, a) >= 0) {
+                x0 = a;
+            }
+            if (func(rownanie, b) * func(f2, b) >= 0) {
+                x0 = b;
+            }
+        }
+        double x1;
+        double xn;
+        if ( x0 == b) {
+            x1=a;
+        }
+        else {
+            x1=b;
+        }
+        do {
+            xn = x1 - (func(rownanie,x1)*(x1-x0))/(func(rownanie,x1)-func(rownanie,x0)) ;
+            x0=x1;
+            x1=xn;
+            licznik++;
+        } while (Math.abs(func(rownanie,xn)) >= epsil);
+        System.out.println("x" + licznik + " = " + xn);
+    }
+
+    public static double[] pochodna(double[] rownanie) {
+        int n = rownanie.length;
+        double[] wspPochodnej = new double[n - 1];
+        for (int i = 1; i < n; i++) {
+            wspPochodnej[i - 1] = rownanie[i] * i;
+        }
+        return wspPochodnej;
+    }
+
+    static double func(double[] wspolczynniki, double x) {
+        double wartosc = 0;
+        for (int i = 0; i < wspolczynniki.length; i++) {
+            wartosc += wspolczynniki[i] * Math.pow(x, i);
+        }
+        return wartosc;
+    }
+
+
+    public static void main(String[] args) {
+        double[] wspolczynniki = {-7, -4, -2, 1}; // wspolczynniki podowane sa w odwrotnej kolejnosci tzn. od wyrazu wolnego do najwyzszej potegi
+        double a = 3; // the left end of the interval
+        double b = 4; // the right end of the interval
+        double epsilon = 0.01;
+        sieczne(a, b, epsilon, wspolczynniki);
+    }
+
+}
